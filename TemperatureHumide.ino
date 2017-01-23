c03d4890cf6386e5e07802109db939bfe427723c
LeBellier <Brusuf@orange.fr>
2017-01-23 14:13:29 +0100

    Programme pour Arduino Uno

--- TemperatureHumide.ino
+++ TemperatureHumide.ino
@@ -4,34 +4,37 @@
 //#define DEBUG_INIT
 
 #include "PersonnalData.h"
-#include <Aspect.h>
-#include <ServersWeb.h> // personnalData
-#include <WifiManager.h> // personnalData
-#include "PagesWeb.h"
+#include <DHT.h>
+
+#define DHTPIN 5     // what digital pin we're connected to
+DHT dht(DHTPIN, DHT11);
+long i = 0;
+String  sH = "";
+String sT = "";
 
 void setup(void) {
   Serial.begin(BAUDRATE);
-
-  pinMode( led, OUTPUT);
-  digitalWrite( led, 0);
-  strip.rgbBlink();
-
-  initWIFI();
-  hasDebugDelay(15);
-  artnet.setArtDmxCallback(onDmxFrame);
-  artnet.begin();
-  initDnsHttpFtpServers();
-  httpServer.on("/pixel", HTTP_GET, pixelRequest);
-  hasDebugDelay(20);
-
-  showIP(WiFi.localIP());
-  digitalWrite( led, 1);
+  String msg = "index;H_min;H_max;h;T_min;T_max;t;";
+  Serial.println(msg);
+  dht.begin();
 }
 void loop() {
-  updateServers();
-  artnet.read();
-  if (animate == true) {
-    startShow(showType);
-    animate = false;
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
   }
+
+  i++;
+  sH = String(h);
+  sH.replace(".", ",");
+  sT = String(t);
+  sT.replace(".", ",");
+  String msg = String(i) + ";35;45;" + sH + ";18;20;" + sT + ";";
+  Serial.println(msg);
+
+  // Wait a few seconds between measurements.
+  delay(30000);
+
 }
